efd64eb7679753a7186e085695b4e5cd752b0525
hongzhouye <>
2023-08-23 16:50:57 -0400

    add KUMP2 energy test

---
+++ pyscf/pbc/mp/test/test_kpoint_ump2.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# Copyright 2021 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import unittest
+import numpy as np
+from pyscf import __config__
+from pyscf.pbc import gto, scf, mp
+
+
+class Diamond_GDF(unittest.TestCase):
+    ''' Diamond 211 kpt sampling with total spin = 2
+        The number of occupied bands is [5,4] for spin up and [3,4] for spin down.
+        The system can thus test both spin polarization and padding.
+    '''
+    @classmethod
+    def setUpClass(cls):
+        cell = gto.Cell()
+        cell.verbose = 4
+        cell.output = '/dev/null'
+        cell.atom = 'C 0 0 0; C 0.8925000000 0.8925000000 0.8925000000'
+        cell.a = '''
+        1.7850000000 1.7850000000 0.0000000000
+        0.0000000000 1.7850000000 1.7850000000
+        1.7850000000 0.0000000000 1.7850000000
+        '''
+        cell.pseudo = 'gth-hf-rev'
+        cell.basis = {'C': [[0, (0.8, 1.0)], [0, (0.4, 1.0)],
+                            [1, (1.0, 1.0)], [1, (0.5, 1.0)]]}
+        cell.precision = 1e-12
+        cell.spin = 2
+        cell.build()
+        kpts = cell.make_kpts((2,1,1))
+        mf = scf.KUHF(cell, kpts=kpts).rs_density_fit(auxbasis='weigend').run()
+        cls.cell = cell
+        cls.mf = mf
+    @classmethod
+    def tearDownClass(cls):
+        cls.cell.stdout.close()
+        del cls.cell, cls.mf
+
+    def test_energy(self):
+        mmp = mp.KMP2(self.mf).run()
+        self.assertAlmostEqual(mmp.e_corr, -0.15980996558761992, 6)
+
+    def test_energy_frozen(self):
+        mmp = mp.KMP2(self.mf, frozen=1).run()
+        self.assertAlmostEqual(mmp.e_corr, -0.12460735942012577, 6)
+
+
+class Diamond_FFTDF(unittest.TestCase):
+    ''' Diamond 211 kpt sampling with total spin = 2
+        The number of occupied bands is [5,4] for spin up and [3,4] for spin down.
+        The system can thus test both spin polarization and padding.
+    '''
+    @classmethod
+    def setUpClass(cls):
+        cell = gto.Cell()
+        cell.verbose = 4
+        cell.output = '/dev/null'
+        cell.atom = 'C 0 0 0; C 0.8925000000 0.8925000000 0.8925000000'
+        cell.a = '''
+        1.7850000000 1.7850000000 0.0000000000
+        0.0000000000 1.7850000000 1.7850000000
+        1.7850000000 0.0000000000 1.7850000000
+        '''
+        cell.pseudo = 'gth-hf-rev'
+        cell.basis = {'C': [[0, (0.8, 1.0)], [0, (0.4, 1.0)],
+                            [1, (1.0, 1.0)], [1, (0.5, 1.0)]]}
+        cell.precision = 1e-10
+        cell.spin = 2
+        cell.build()
+        cell.mesh = [21] * 3
+        kpts = cell.make_kpts((2,1,1))
+        mf = scf.KUHF(cell, kpts=kpts).run()
+        cls.cell = cell
+        cls.mf = mf
+    @classmethod
+    def tearDownClass(cls):
+        cls.cell.stdout.close()
+        del cls.cell, cls.mf
+
+    def test_energy(self):
+        mmp = mp.KMP2(self.mf).run()
+        self.assertAlmostEqual(mmp.e_corr, -0.159839676890340, 6)
+
+    def test_energy_frozen(self):
+        mmp = mp.KMP2(self.mf, frozen=1).run()
+        self.assertAlmostEqual(mmp.e_corr, -0.124626982939688, 6)
+
+
+if __name__ == "__main__":
+    print("Full Tests for KUMP2")
+    unittest.main()
