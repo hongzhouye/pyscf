d33c8c1cc2fefc41fb37f7c054569414da7fd92a
hongzhouye <>
2023-08-23 16:13:31 -0400

    add KUMP2 energy

--- pyscf/pbc/mp/__init__.py
+++ pyscf/pbc/mp/__init__.py
@@ -16,6 +16,7 @@
 from pyscf.pbc import scf
 from pyscf.pbc.mp import mp2
 from pyscf.pbc.mp import kmp2
+from pyscf.pbc.mp import kump2
 from pyscf.pbc.mp import kmp2_ksymm
 from pyscf.pbc.lib import kpts as libkpts
 
@@ -33,10 +34,31 @@ def GMP2(mf, frozen=None, mo_coeff=None, mo_occ=None):
     mf = scf.addons.convert_to_ghf(mf)
     return mp2.GMP2(mf, frozen, mo_coeff, mo_occ)
 
+# TODO: separate KRHF/KUHF etc like in molecular MP2
+def KMP2(mf, frozen=None, mo_coeff=None, mo_occ=None):
+    if isinstance(mf, scf.kuhf.KUHF):
+        return KUMP2(mf, frozen, mo_coeff, mo_occ)
+    elif isinstance(mf, scf.kghf.KGHF):
+        return KGMP2(mf, frozen, mo_coeff, mo_occ)
+    else:
+        return KRMP2(mf, frozen, mo_coeff, mo_occ)
+
 def KRMP2(mf, frozen=None, mo_coeff=None, mo_occ=None):
     if isinstance(mf.kpts, libkpts.KPoints):
         return kmp2_ksymm.KRMP2(mf, frozen, mo_coeff, mo_occ)
     else:
         return kmp2.KRMP2(mf, frozen, mo_coeff, mo_occ)
 
-KMP2 = KRMP2
+def KUMP2(mf, frozen=None, mo_coeff=None, mo_occ=None):
+    if isinstance(mf.kpts, libkpts.KPoints):
+        raise NotImplementedError
+        return kump2_ksymm.KUMP2(mf, frozen, mo_coeff, mo_occ)
+    else:
+        return kump2.KUMP2(mf, frozen, mo_coeff, mo_occ)
+
+def KGMP2(mf, frozen=None, mo_coeff=None, mo_occ=None):
+    if isinstance(mf.kpts, libkpts.KPoints):
+        raise NotImplementedError
+        return kgmp2_ksymm.KGMP2(mf, frozen, mo_coeff, mo_occ)
+    else:
+        return kgmp2.KGMP2(mf, frozen, mo_coeff, mo_occ)
